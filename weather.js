92adfdc2f06a1aae8edd0a48817010dcca7a32d0
JacobWylie <jacobwylie@gmail.com>
2017-03-21 13:07:51 +0100

    added error handling

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const https = require('https');
 const api = require('./api.json');
 
@@ -7,22 +8,73 @@ const printWeather = (weather) => {
 	console.log(message);
 }
 // Print out error message
+const printError = (error) => {
+	console.error(error.message);
+}
 
 const get = (query) => {
-	const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`,
+	const readableQuery = query.replace('_', ' ');
+	try {
+		const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`,
 		response => {
-			let body = '';
-			// Read the data
-			response.on('data', chunk => {
-				body += chunk;
-			});
-			response.on('end', () => {
-				// parse data
-				const weather = JSON.parse(body);				
-				// print the data
-				printWeather(weather);
-			});
+			if(response.statusCode === 200) {
+				let body = '';
+				// Read the data
+				response.on('data', chunk => {
+					body += chunk;
+				});
+				response.on('end', () => {
+					try {
+						// parse data
+						const weather = JSON.parse(body);
+						if(weather.location) {				
+							// print the data
+							printWeather(weather);
+						} else {
+							const queryError = new Error(`The location "${readableQuery}" was not found.`);
+							printError(queryError);
+						}	
+					} catch (error) {
+						// parse error
+						printError(error);
+					}
+				});
+			} else {
+				// Status code error
+				const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${https.STATUS_CODES[response.statusCode]})`);
+				printError(statusCodeError);
+			}	
 		});
+		request.on('error', printError);
+	} catch (error) {
+		printError(error);
+	}	
 }
 
 module.exports.get = get;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
